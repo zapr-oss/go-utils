0016df476e8009b8bab72db0c7dd93796f7c9a3e
Akash Sharma S <akash@zapr.in>
2020-04-08 19:51:23 +0530

    Add transaction methods.

--- entities/mysql/mysql_impl.go
+++ entities/mysql/mysql_impl.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"log"
 	"strings"
 	"time"
 )
@@ -30,4 +31,31 @@ func (entity *Entity) executeQueryWithRetry(tx *sql.Tx, query string, iterationC
 		}
 	}
 	return res, err
+}
+
+func (entity *Entity) StartTransaction() *sql.Tx {
+	tx, err := entity.DB.Begin()
+	if err != nil {
+		log.Println("Failed to start transaction on DB.", err)
+		return nil
+	}
+	return tx
+}
+
+func (entity *Entity) CommitTransaction(tx *sql.Tx) bool {
+	err := tx.Commit()
+	if err != nil {
+		log.Println("Failed to commit transaction on DB.", err)
+		return false
+	}
+	return true
+}
+
+func (entity *Entity) RollbackTransaction(tx *sql.Tx) bool {
+	err := tx.Rollback()
+	if err != nil {
+		log.Println("Failed to rollback transaction on DB.", err)
+		return false
+	}
+	return true
 }
